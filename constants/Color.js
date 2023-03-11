d0b47794907e3f2544c090548a40e191187c7fc3
Thanistas <thanithanis171198@gmail.com>
2023-03-11 20:21:31 +0530

    Initial setup

---
+++ constants/Color.js
@@ -0,0 +1,7 @@
+export const PrimaryColor = "#032F41"
+// export const SecondaryColor = "#E83E52"
+
+export const TextPrimaryColor = "#282828"
+export const TextSecondaryColor = "#282828"
+
+export const BgWhiteColor = '#F6F6F6'
